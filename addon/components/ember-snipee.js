844bb8c54c653bb63d2c03bea78f9281ef35b273
Damian Senn <damian.senn@adfinis-sygroup.ch>
2016-01-25 09:29:38 +0100

    Move rectangle html generation into Rectangle class

--- addon/components/ember-snipee.js
+++ addon/components/ember-snipee.js
@@ -4,14 +4,7 @@ import computed, {
   oneWay
 } from 'ember-computed'
 
-import {
-  htmlSafe
-} from 'ember-string'
-
-import Rectangle, {
-  ZERO_RECTANGLE
-} from '../lib/rectangle'
-
+import Rectangle        from '../lib/rectangle'
 import computedInt      from '../lib/computed-int'
 import computedSnapAxis from '../lib/computed-snap-axis'
 
@@ -51,18 +44,15 @@ export default Component.extend({
   },
 
   style: computed('x1', 'y1', 'x2', 'y2', function style() {
-    let x1 = this.get('x1')
-    let y1 = this.get('y1')
-    let x2 = this.get('x2')
-    let y2 = this.get('y2')
-
-    this.sendAction('on-draw', new Rectangle(x1, y1, x2, y2))
-
-    return htmlSafe(
-      `top:${y1}px;` +
-      `left:${x1}px;` +
-      `width:${x2 - x1}px;` +
-      `height:${y2 - y1}px`
+    let rectangle = new Rectangle(
+      this.get('x1'),
+      this.get('y1'),
+      this.get('x2'),
+      this.get('y2')
     )
+
+    this.sendAction('on-draw', rectangle)
+
+    return rectangle.html
   }).readOnly()
 })
--- addon/lib/rectangle.js
+++ addon/lib/rectangle.js
@@ -1,10 +1,33 @@
-export default function Rectangle(x1, y1, x2, y2) {
-  this.x1 = x1 | 0
-  this.y1 = y1 | 0
-  this.x2 = x2 | 0
-  this.y2 = y2 | 0
+import {
+  htmlSafe
+} from 'ember-string'
 
-  //Object.freeze(this)
+export default class Rectangle {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1 | 0
+    this.y1 = y1 | 0
+    this.x2 = x2 | 0
+    this.y2 = y2 | 0
+
+    //Object.freeze(this)
+  }
+
+  get width() {
+    return this.x2 - this.x1
+  }
+
+  get height() {
+    return this.y2 - this.y1
+  }
+
+  get html() {
+    return htmlSafe(
+      `top:${this.y1}px;` +
+      `left:${this.x1}px;` +
+      `width:${this.width}px;` +
+      `height:${this.height}px`
+    )
+  }
 }
 
 if (Symbol.toStringTag) {
